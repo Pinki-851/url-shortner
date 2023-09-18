06fe710522723d403c7f7b44441416cfc533b1d4
Pinki-851 <pinkisaroj942@gmail.com>
2023-09-18 14:07:02 +0530

    test removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,22 @@
-import '../styles/globals.css'
-import type { Metadata } from 'next'
-import {  Poppins } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import { ReactNode } from 'react';
+import '../styles/globals.css';
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: '400'
-})
+  weight: '400',
+});
 
 export const metadata: Metadata = {
   title: 'URL shortner',
   description: 'url shortner',
+};
 
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={poppins.className}>{children}</body>
     </html>
-  )
+  );
 }
